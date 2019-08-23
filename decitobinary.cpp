5a1625584f33abc4457e73d00a2a7ae15c691b85
erikasan <erikasan@student.matnat.uio.no>
2019-08-23 15:25:07 +0200

    Adding a bunch of simple programs in C++

---
+++ decitobinary.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  short int i;
+  vector<short int> digs;
+  cout << "Enter an integer: ";
+  cin >> i;
+  while (i > 0){
+    digs.push_back(i%2);
+    i = i/2;
+  }
+  for (short int e = digs.size() - 1; e >= 0; --e){
+    cout << digs[e];
+  }
+}
+
