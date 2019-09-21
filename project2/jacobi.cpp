1574a311b30e3a9a88070d57dc86b79e9a08593a
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-21 10:28:46 +0200

    Added simplification for symmetric matrix in find_a_pq()

--- project2/jacobi.cpp
+++ project2/jacobi.cpp
@@ -5,16 +5,20 @@ using namespace std;
 using namespace arma;
 
 void find_a_pq(unsigned int, mat, double*, unsigned int*, unsigned int*);
+void rotate();
 
 int main()
 {
-  unsigned int n = 10;
+  unsigned int n = 3;
   unsigned int p, q;
   double a_pq;
   Mat<double> A(n, n);
   A.randu();
-  A(1, 0) = 10;
+  A(0, 1) = 69;
   find_a_pq(n, A, &a_pq, &p, &q);
+  cout << A << endl;
+  cout << p << ' ' << q << endl;
+  cout << a_pq << endl;
   return 0;
 }
 
@@ -27,19 +31,11 @@ void find_a_pq(unsigned int n,
 {
   *a_pq = 0;
   for (unsigned int i = 0; i != n; ++i) {
-    for (unsigned int j = 0; j != n; ++j) {
-      if (i != j) {
-        if (abs(A(i, j) > *a_pq)) {
-          *a_pq = abs(A(i, j));
-          if (i < j) {
-            *p = i;
-            *q = j;
-          }
-          else {
-            *p = j;
-            *q = i;
-          }
-        }
+    for (unsigned int j = i + 1; j != n; ++j) {
+      if (abs(A(i, j) > *a_pq)) {
+        *a_pq = abs(A(i, j));
+        *p = i;
+        *q = j;
       }
     }
   }
