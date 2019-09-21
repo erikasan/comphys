46f1fe54a3a60ef76d48df06c6b90f8769658444
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-21 11:42:00 +0200

    Rotate function works

--- project2/jacobi.cpp
+++ project2/jacobi.cpp
@@ -1,24 +1,26 @@
 #include <iostream>
+#include <cmath>
 #include <armadillo>
 
 using namespace std;
 using namespace arma;
 
 void find_a_pq(unsigned int, mat, double*, unsigned int*, unsigned int*);
-void rotate();
+void rotate(unsigned int, mat&, unsigned int, unsigned int);
 
 int main()
 {
-  unsigned int n = 3;
+  unsigned int n = 2;
   unsigned int p, q;
   double a_pq;
   Mat<double> A(n, n);
-  A.randu();
-  A(0, 1) = 69;
-  find_a_pq(n, A, &a_pq, &p, &q);
+  vec row = randi<vec>(n);
+  A = toeplitz(row, row);
   cout << A << endl;
-  cout << p << ' ' << q << endl;
-  cout << a_pq << endl;
+  cout << endl;
+  find_a_pq(n, A, &a_pq, &p, &q);
+  rotate(n, A, p, q);
+  cout << A << endl;
   return 0;
 }
 
@@ -39,5 +41,33 @@ void find_a_pq(unsigned int n,
       }
     }
   }
+  return;
+}
+
+void rotate(unsigned int n, mat& A, unsigned int p, unsigned int q)
+{
+  double tau, t, c, s;
+  mat S = eye(n, n);
+  if (A(p, q) != 0) {
+    tau = (A(q, q) - A(p, p))/(2*A(p, q));
+
+    if (tau >= 0) {
+      t = 1/(tau + sqrt(1 + tau*tau));
+    }
+    else {
+      t = 1/(tau - sqrt(1 + tau*tau));
+    }
+    c = 1/sqrt(1 + t*t);
+    s = c*t;
+  }
+  else {
+    c = 1; s = 0;
+  }
+
+  S(p, p) = c; S(p, q) = s;
+  S(q, p) = -s; S(q, q) = c;
+
+  A = trans(S)*(A*S);
+
   return;
 }
