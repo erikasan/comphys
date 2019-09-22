df40735c14ad4f9275b38966e059587a8d7c5b2b
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-22 19:47:46 +0200

    New python program for visualising the performance of the Jacobi method

---
+++ project2/plot_iterations.py
@@ -0,0 +1,30 @@
+import numpy as np, matplotlib.pyplot as plt
+
+data = np.loadtxt('cnt_iterations.dat');
+
+n = data[:, 0]; iterations = data[:, 1]; CPUtime = data[:, 2]
+
+'''
+# Plot # of iterations vs matrix dimension
+plt.style.use('fivethirtyeight')
+plt.grid()
+plt.grid()
+plt.plot(n, iterations)
+plt.tight_layout()
+plt.show()
+'''
+
+# Plot CPU time vs matrix dimension
+plt.style.use('fivethirtyeight')
+plt.grid()
+plt.grid()
+plt.plot(n, CPUtime)
+plt.tight_layout()
+plt.show()
+
+# Iterations, least squares and error analysis
+p, error = np.polyfit(n, iterations, 3, cov = True)
+
+
+# CPUtime, least squares and error analysis
+p, error = np.polyfit(n, CPUtime, 3, cov = True)
