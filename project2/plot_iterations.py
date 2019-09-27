4a6ca305afcf5ff701c5c2d22e1e9bdac1323f08
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-27 15:40:52 +0200

    No clue what is going on. At least plt.style.use works again

--- project2/plot_iterations.py
+++ project2/plot_iterations.py
@@ -1,18 +1,20 @@
 import numpy as np, matplotlib.pyplot as plt
-import matplotlib
+
 
 data = np.loadtxt('cnt_iterations.dat');
 
 n = data[:, 0]; iterations = data[:, 1]; CPUtime = data[:, 2]
 p = np.polyfit(n, iterations, 3)
 
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
 
 plt.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0, 0), useMathText = True)
 plt.style.use('fivethirtyeight')
-plt.plot(n, iterations, marker = 'o')
-plt.plot(n, p[0]*n**3 + p[1]*n**2 + p[2]*n + p[3], alpha = 0.4)
+plt.plot(n, iterations, marker = 'o', color = colors[0])
+plt.plot(n, p[0]*n**3 + p[1]*n**2 + p[2]*n + p[3], alpha = 0.5, color = colors[1])
 plt.xlabel('Matrix dimension n'); plt.ylabel('# of iterations')
-#plt.grid()
+plt.grid()
 plt.grid()
 plt.tight_layout()
 plt.show()
