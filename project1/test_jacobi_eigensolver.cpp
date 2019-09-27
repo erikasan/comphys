756a06ec10c3063e2e88b1a16aac1907568d987d
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-27 15:41:18 +0200

    Will add tests later

---
+++ project1/test_jacobi_eigensolver.cpp
@@ -0,0 +1,3 @@
+// Compile & run:
+// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -o test_jacobi_eigensolver test_jacobi_eigensolver.cpp CatchMain.o && test_jacobi_eigensolver --success
+// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% test_jacobi_eigensolver.cpp CatchMain.obj && test_jacobi_eigensolver --success
