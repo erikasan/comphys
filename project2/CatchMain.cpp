de4d21ddea07b2c5308c7d20997aed4b76490360
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-09-27 14:32:41 +0200

    Added CatchMain.cpp

---
+++ project2/CatchMain.cpp
@@ -0,0 +1,7 @@
+#define CATCH_CONFIG_MAIN
+
+#include <catch2/catch.hpp>
+
+// Compile implementation of Catch for use with files that do contain tests:
+// - g++ -std=c++11 -Wall -I$(CATCH_SINGLE_INCLUDE) -c 000-CatchMain.cpp
+// - cl -EHsc -I%CATCH_SINGLE_INCLUDE% -c 000-CatchMain.cpp
