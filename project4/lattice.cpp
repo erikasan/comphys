112c0cff6add6faf7fb7db999cd41e299298c156
Erik Alexander Sandvik <erikasan@student.matnat.uio.no>
2019-11-08 12:37:37 +0100

    Added initialize function to account for periodic boundary conditions

--- project4/lattice.cpp
+++ project4/lattice.cpp
@@ -5,7 +5,9 @@
 using namespace std;
 using namespace arma;
 
-void initialize(imat &spins, int N);
+void initialize(int N, imat &spins, vec w);
+void metropolis(int N, imat spins);
+inline int periodic(int index, int limit);
 
 int main()
 {
@@ -13,7 +15,7 @@ int main()
   int N = 100; int E = 0;
   //imat spins(N, N); spins.ones();
 
-  imat spins(N, N);
+  imat spins(N, N); vec w(17);
 
   initialize(spins, N);
 
@@ -37,7 +39,7 @@ int main()
   return 0;
 }
 
-void initialize(imat &spins, int N)
+void initialize(int N, imat &spins, vec w)
 {
 
   random_device rd;
@@ -54,4 +56,37 @@ void initialize(imat &spins, int N)
 
   cout << spins << endl;
   return;
+}
+
+void metropolis(imat spins, int N)
+{
+
+  random_device rd;
+  mt19937_64 gen(rd());
+  uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
+
+  int l, k;
+
+  double dE;
+
+  for (int i = 0; i < N; ++i) {
+  for (int j = 0; j < N; ++j) {
+
+    l = (int) RandomNumberGenerator(gen)*N;
+    k = (int) RandomNumberGenerator(gen)*N;
+
+    dE = 2*spins(l, k)* (   spins(periodic(l - 1, N), k)
+                          + spins(periodic(l + 1, N), k)
+                          + spins(l, periodic(k - 1, N))
+                          + spins(l, periodic(k + 1, N))
+                        )
+    }}
+
+
+
+}
+
+inline int periodic(int index, int limit){
+  // Thanks to Daniel Heinesen!!
+  return (index + limit)%(limit);
 }
